5ce34d0cfb1589314d605d021314644b81508e87
mm12346 <xoctell338@gmail.com>
2026-04-18 17:01:10 +0700

    Update CACHE_NAME to version 4

--- admin-sw.js
+++ admin-sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Check Log Admin PWA
-const CACHE_NAME = 'checklog-admin-v3.0.3.3';
+const CACHE_NAME = 'checklog-admin-v4';
 const urlsToCache = [
     './',
     './index.html',
