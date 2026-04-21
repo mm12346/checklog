a3fd81f28afe45f0e7cff61ac11ee651e3bee663
mm12346 <xoctell338@gmail.com>
2026-04-21 14:04:42 +0700

    Update admin-sw.js

--- admin-sw.js
+++ admin-sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Check Log Admin PWA
-const CACHE_NAME = 'checklog-admin-v4.0.0';
+const CACHE_NAME = 'checklog-admin-v4.1';
 const urlsToCache = [
     './',
     './index.html',
